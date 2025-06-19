9a4d77d21df7b0fbfc7180d8dbe8e72b5b250adc
sahil-mangla <manglasahil2017@gmail.com>
2025-06-20 01:08:46 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,19 @@ document.querySelectorAll('.js-add-to-cart')
             productId: productId,
             quantity: 1,
           });
-        console.log(cart);
+
+          let cartQuantity = 0;
+
+          cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+          })
+        
+        
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
         }
+
+
+
     });
 });

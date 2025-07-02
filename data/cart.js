337af2393804a49b6fb04adbc346472a7f76cd46
sahil-mangla <manglasahil2017@gmail.com>
2025-07-02 23:06:42 +0530

    Create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,27 @@
-export let cart = JSON.parse(localStorage.getItem('cart')); 
+export let cart;
 
-if(!cart){
-  cart = [{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 2,
-  deliveryOptionId: '1',
-},{
-  productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-  quantity: 1,
-  deliveryOptionId: '2',
-}];
-}
+loadFromStorage();
+
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart')); 
+  
+  if(!cart){
+    cart = [{
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 2,
+      deliveryOptionId: '1',
+    },{
+      productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+      quantity: 1,
+      deliveryOptionId: '2',
+    }];
+  }
+};
 
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart));
 };
-
-
-
 
 
 export function addToCart(productId){

5f1b415002a69316b982d88082878cb0c7ce5904
sahil-mangla <manglasahil2017@gmail.com>
2025-07-03 23:30:33 +0530

    Use inheritence and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -35,7 +35,53 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`
   }
 
+  extraInfoHTML(){
+    return '';
+  }
+
 }
+
+class Clothing extends Product{ 
+    sizeChartLink;
+
+    constructor(productDetails) {
+      super(productDetails); //calls the constructor of parent class
+      this.sizeChartLink = productDetails.sizeChartLink;
+
+    }
+
+    extraInfoHTML(){
+      // super.extraInfoHTML();
+      return `
+        <a href="${this.sizeChartLink} target="_blank">Size Chart</a>
+      `;
+    }
+
+}
+
+  const tshirt = new Clothing({
+      id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+      name: "Adults Plain Cotton T-Shirt - 2 Pack",
+      rating: {
+        stars: 4.5,
+        count: 56
+      },
+      priceCents: 799,
+      keywords: [
+        "tshirts",
+        "apparel",
+        "mens"
+      ],
+      type: "clothing",
+      sizeChartLink: "images/clothing-size-chart.png"
+    });
+
+console.log(tshirt);
+console.log(tshirt.getPrice());
+
+
+
 
 //example to understand
 const product1 = new Product({
@@ -716,6 +762,11 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
+
   return new Product(productDetails); 
 });
 

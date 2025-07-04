3dba2003a8d1d516d326f1ca69e63ed874558776
sahil-mangla <manglasahil2017@gmail.com>
2025-07-04 22:18:31 +0530

    Learn Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,60 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/backend-practise.js'
 // import '../data/cart-class.js';
 
+Promise.all([
+    new Promise((resolve)=>{
+    //runs the inner function immediatelty 
+    loadProducts(()=>{
+        resolve('value1'); //load products
+    });
+}),
 
-loadProducts(() =>{
-    renderOrderSummary();
+new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve(); //load cart
+        });
+})
+
+]).then((values)=>{
+    console.log(values);
+    
+    renderOrderSummary(); 
     renderPaymentSummary();
 });
 
+/*
+new Promise((resolve)=>{
+    //runs the inner function immediatelty 
+    loadProducts(()=>{
+        resolve('value1'); //load products
+    });
+    
+}).then((value)=>{
+    console.log(value);
+    
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve(); //load cart
+        });
+    });
+
+}).then(()=>{
+    renderOrderSummary(); //render other sh
+    renderPaymentSummary();
+});
+*/
+
+/* 
+Too much nesting to deal with on longer run
+
+loadProducts(() =>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
